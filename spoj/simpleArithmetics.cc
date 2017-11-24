eacbac5d3730cea4e7cc6b575fea5945927cc89c
PranavSharma97 <reachtopranav@yahoo.co.in>
2017-11-23 22:27:47 -0500

    refactored SimpleArithmetics.cc

--- spoj/simpleArithmetics.cc
+++ spoj/simpleArithmetics.cc
@@ -41,7 +41,9 @@ int main()
         ca=a;
         cb=b;
         int tempCarry=0;
-        string carry(1000,'0'),prod(1000,'0'),sum(510,'0'),diff(510,'0');
+//         string carry(1000,'0');
+        string prod(1000,'0'),sum(510,'0'),diff(510,'0');
+        vector<int> carry(1000,0);
         vector<vector <int> > res(500,vector<int>(1000,-1));
         reverse(a.begin(),a.end());
         reverse(b.begin(),b.end()); 
@@ -56,6 +58,9 @@ int main()
                     
                     if(y==0)
                     {
+                        carry[i+j+1] = (prod[i+j] - 48 + carry[i+j])/10 + carry[i+j+1];
+                        prod[i+j] = to_string((prod[i+j] - 48 + carry[i+j])%10)[0];
+                        carry[i+j] = 0;
                         res[i][j+i]=0;
                         break;
                     }
@@ -74,14 +79,18 @@ int main()
                     }
                     
 //                    carry[i+j+2] = to_string(((prod[i+j] - 96 + rem + carry[i+j])/10 + carry[i+j+1] - 48)/10)[0];
-                    carry[i+j+1] = to_string(((prod[i+j] - 96 + rem + carry[i+j])/10 + carry[i+j+1] - 48)%10)[0];
                     
-                    prod[i+j] = to_string((prod[i+j] - 96 + rem + carry[i+j])%10)[0];
-                    carry[i+j] = '0';
+//                     carry[i+j+1] = to_string(((prod[i+j] - 96 + rem + carry[i+j])/10 + carry[i+j+1] - 48)%10)[0];
+                    
+                    carry[i+j+1] = (prod[i+j] - 48 + rem + carry[i+j])/10 + carry[i+j+1];
+                    
+                    prod[i+j] = to_string((prod[i+j] - 48 + rem + carry[i+j])%10)[0];
+//                     carry[i+j] = '0';
+                    carry[i+j] = 0;
                     if(j==a.length()-1 && i==b.length()-1)
                     {
-                        int next = (prod[i+j+1] + carry[i+j+1] - 96)/10;
-                        prod[i+j+1] = to_string((prod[i+j+1] + carry[i+j+1] - 96)%10)[0]; 
+                        int next = (prod[i+j+1] + carry[i+j+1] - 48)/10;
+                        prod[i+j+1] = to_string((prod[i+j+1] + carry[i+j+1] - 48)%10)[0]; 
                         if(next!=0)
                         {
                             prod[i+j+2] = next + 48;
